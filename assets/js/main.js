44b427ad8cdc366f46e68894b42091f80a9cb421
Symon <symonlima07@gmail.com>
2025-04-30 18:43:47 -0300

    Mudanças

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,7 +33,59 @@ function updateSoftSkills(profileData) {
 };
 
 function updateHardSkills(profileData) {
-    
+
+    const hardSkills = document.getElementById('profile.skills.hardSkills');
+
+    hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`).join('');
+
+}
+
+function updateLanguages(profileData) {
+
+    const languages = document.getElementById('profile.languages');
+
+    languages.innerHTML = profileData.languages.map(language => `<li>${language}</li>`).join('');
+
+}
+
+function updatePortfolio(profileData) {
+
+    const portfolio = document.getElementById('profile.portfolio');
+
+    portfolio.innerHTML = profileData.portfolio.map(project => {
+        return `
+        <li>
+            <span ${project.github ? 'class="title github"' : 'title'}>${project.name}</span>
+            <a href="${project.url}" target="_blank" rel="noopener">${project.url}</a>
+        </li>
+        `
+    }).join('');
+
+}
+
+function updateProfessionalExperience(profileData) {
+    const professionalExperience = document.getElementById('profile.professionalExperience');
+
+    professionalExperience.innerHTML = profileData.professionalExperience.map(experience => {
+        
+        let descriptionContent;
+        
+        if (typeof experience.description === 'object' && experience.description !== null) {
+            descriptionContent = Object.entries(experience.description)
+                .map(([key, value]) => `<p>${value}</p>`)
+                .join('');
+        } else {
+            descriptionContent = `<p>${experience.description || ''}</p>`;
+        }
+
+        return `
+            <li>
+                <h3 class="title">${experience.name}</h3>
+                <span class="period">${experience.period}</span>
+                ${descriptionContent}
+            </li>
+        `;
+    }).join('');
 }
 
 (async () => {
@@ -41,5 +93,9 @@ function updateHardSkills(profileData) {
     const profileData = await fetchProfileData();
     updateProfileInfo(profileData);
     updateSoftSkills(profileData);
+    updateHardSkills(profileData);
+    updateLanguages(profileData);
+    updatePortfolio(profileData);
+    updateProfessionalExperience(profileData);
 
 })();
