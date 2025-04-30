696617898d64e87fb8f6bfba1c215dd9f7df78a4
Symon <symonlima07@gmail.com>
2025-04-30 19:41:48 -0300

    Voltando o / na importação.

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,7 @@
 
 async function fetchProfileData() {
     
-    const url = 'https://raw.githubusercontent.com/SymonSL7/Portfolio/refs/heads/mainassets/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/SymonSL7/Portfolio/refs/heads/main/assets/data/profile.json';
 
     const fetching = await fetch(url);
 
